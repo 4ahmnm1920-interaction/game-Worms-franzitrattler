5b2ab6519234feb90e1d12dc29c02b69861c1765
franzitrattler <franziska.trattler@gmail.com>
2020-02-05 11:27:13 +0100

    move left right and up

---
+++ Assets/MyGame/Scripts/WormsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public Vector3 jump;
+    public Vector3 left;
+    public Vector3 right;
+
+    public KeyCode shootKey;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(jump);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForce(left);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            rb.AddForce(right);
+        }
+
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            Debug.Log("boom");
+        }
+    }
+
+}
